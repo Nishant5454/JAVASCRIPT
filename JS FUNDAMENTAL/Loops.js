c116093b2191bee6be68d13cee41be03f0d7903f
Nishant5454 <bhagatnishant5454@gmail.com>
2023-08-28 10:10:16 +0530

    loops

---
+++ JS FUNDAMENTAL/Loops.js
@@ -0,0 +1,44 @@
+/*for (let index = 0; index < 10; index++) { 
+    console.log(`value is ${index}`);
+
+    
+}  
+
+const names=["nishant","suraj","vibhuti"]; 
+let another=[];
+for(let i=0;i<names.length;i++){  
+    const ele=names[i]
+      another.push(ele.toUpperCase()); 
+    
+} 
+console.log(another); 
+ 
+let i=10; 
+while(i<100){ 
+    console.log(`Value is${i}`); 
+    i++; 
+
+} 
+
+let i=0; 
+do{  
+    console.log(`value of i is ${i}`);
+    i++; 
+
+}while(i<10) 
+
+let arr=["hulk","hammer","thor"];  
+let another=[];
+for(let i=0;i<arr.length;i++){ 
+    console.log(`The value of of arr is ${i}`); 
+    const ele=arr[i];  
+    another.push(ele.toUpperCase()); 
+
+
+} 
+console.log(another); 
+*/ 
+const language=["cpp","python"] 
+for (const lan of language) {
+    console.log(`The value of language is ${lan.toUpperCase()}`);
+}
