d5b4e592dff30eec9e7c169e80a6cd5b5ab76604
Nishant5454 <bhagatnishant5454@gmail.com>
2023-09-02 14:01:14 +0530

    callbacks

--- Prototyping/callba.js
+++ Prototyping/callba.js
@@ -30,13 +30,29 @@ function dummy(){
     return new Promise((resolve, reject) => {
         setTimeout(function time(){ 
             console.log("WORK DONE");
-        },1000);
-        resolve("Work done")
+        },1000); 
+        let randomno=Math.random; 
+        if(randomno>0.5) 
+        resolve("Work done") 
+    else 
+    reject("err")
     })
 } 
 let p=dummy();  
 p.then(function time(val){ 
     console.log("Valus is resolved",val);
+})  
+p. 
+catch(function err(err){ 
+    console.log("Error resoleve ",err);
 }) 
+p. 
+finally(function exec(ex){ 
+    console.log("Value is Executed ",ex);
+}) 
+p. 
+then(function ter(teri){ 
+    console.log("Value is Not traced",teri);
+})
 for(let i=0;i<1000000000;i++){} 
 console.log("End position");
